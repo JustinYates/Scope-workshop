abc3f601b1db6491a73b8d068dc226ab54604778
JustinYates <Justin.yates24601@gmail.com>
2018-01-29 15:55:59 -0500

    new

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,40 @@
 package com.company;
 
 public class Main {
+    String membervariable = "i am a member variable, i have class level scope";
+    Main main = new Main();
+    main.aMethod
 
     public static void main(String[] args) {
-	// write your code here
+
+
+    }
+
+    private void aMethod() {
+        String localvariable = "i am a local variable i can only ber used in this variable";
+        System.out.println("inside first method");
+        System.out.println(membervariable);
+        System.out.println(localvariable);
+        private void anothermethod() {
+            System.out.println("inside second method");
+        }
+
+    }
+    private void methodForLoopVariables () {
+        String localVariable = "just another member variable inside our main loop";
+        System.out.print(localVariable);
+        boolean runDoLoop = true;
+        do {
+            String loopVariable = "i am a loop variable inside of our do while loop";
+            System.out.println("inside our do while loop:");
+            System.out.println(localVariable);
+            runDoLoop = false;
+        } while (runDoLoop);
+        System.out.println("inside thirf method, just outside do while loop");
+        System.out.println(localVariable);
+        System.out.println();
+
+
     }
 }
+
